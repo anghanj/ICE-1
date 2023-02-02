8dda6fe7e16b3dc0e08abe68afb6d2799dcd9ff4
anghanj <anghanj@sheridancollege.ca>
2023-02-01 19:56:40 -0500

    ICE-1 branch version 1

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,5 +1,8 @@
 package exercise1;
 
+import java.util.Scanner;
+import java.util.Random;
+
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
@@ -13,26 +16,34 @@ public class CardTrick {
     public static void main(String[] args) {
         
         Card[] hand = new Card[7];
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
 
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
-            //       Don't worry about duplicates at this point
+            card.setValue(random.nextInt(13) + 1);
+            card.setSuit(Card.SUITS[random.nextInt(4)]);
+            hand[i] = card;
         }
 
-        // insert code to ask the user for Card value and suit, create their card
-        // and search the hand here. 
-        // Hint: You can ask for values 1 to 10, and then
-        //       11 for jack, 12 for queen, etc. (remember arrays are 0-based though)
-        //       1 for Hearts, 2 for Diamonds, etc. (remember arrays are 0-based though)
-        // 
-        // Then loop through the cards in the array to see if there's a match.
-        
-        // If the guess is successful, invoke the printInfo() method below.
-        
+        System.out.println("Pick a card (value between 1 and 13)");
+        int value = scanner.nextInt();
+        System.out.println("Pick a suit (1 for Hearts, 2 for Diamonds, 3 for Spades, 4 for Clubs)");
+        int suit = scanner.nextInt();
+
+        boolean a = false;
+        for (int i = 0; i < hand.length; i++) {
+            if (hand[i].getValue() == value && hand[i].getSuit().equals(Card.SUITS[suit - 1])) {
+                a = true;
+                break;
+            }
+        }
+
+        if (a) {
+            printInfo();
+        } else {
+            System.out.println("The card was not found in the hand");
+        }
     }
 
     /**
@@ -51,8 +62,8 @@ public class CardTrick {
         System.out.println("My career ambitions:");
         System.out.println("-- Be more active on LinkedIn");
         System.out.println("-- Have a semester with no violations of academic integrity!");
-	System.out.println();	
-
+        System.out.println();
+        
         System.out.println("My hobbies:");
         System.out.println("-- Investing");
         System.out.println("-- Cooking");
@@ -60,8 +71,6 @@ public class CardTrick {
         System.out.println("-- Riding my motorcycle");
 
         System.out.println();
-        
-    
     }
 
 }
